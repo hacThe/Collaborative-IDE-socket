f49f791d9d991ce8f73040fe04ec2f0635701d36
hacthe-codelink <the.duong@codelink.io>
2022-11-23 09:24:09 +0700

    fix server.js

---
+++ temp/56031ld/Main.java
@@ -0,0 +1,22 @@
+class Main
+{
+public static void main(String[] args)
+{
+System.out.println("Hello World");
+}
+}
+
+
+import java.util.*;
+public class Main {
+ 
+  public static void main(String[] args) {        
+        String a = new String();
+        Scanner x = new Scanner(System.in );
+        a = x.next();
+        System.out.println(a);
+            
+ 
+    }
+ 
+}
