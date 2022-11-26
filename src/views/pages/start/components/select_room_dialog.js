86cf66c57215f8069b25bfe5019f7bbd086ae5f3
Lam Truong <lamtk.84@gmail.com>
2022-11-27 00:09:54 +0700

    handle calling api for checking existing room

--- src/views/pages/start/components/select_room_dialog.js
+++ src/views/pages/start/components/select_room_dialog.js
@@ -13,8 +13,10 @@ import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { setUserName } from "../../../../redux/slices/app-slice";
+import axios from 'axios';
 
 function SelectRoomDialog({ open, handleClose }) {
+  const [error, setError] = useState("");
   const [roomId, setRoomId] = useState("");
   const navigate = useNavigate();
   return (
@@ -32,6 +34,7 @@ function SelectRoomDialog({ open, handleClose }) {
           onChange={(e) => setRoomId(e.target.value)}
           type="text"
         />
+        <h4 style={{ color: "red", display: "flex", justifyContent: "right", alignItems: "right", marginTop: "4px" }}>{error}</h4>
         <Box sx={{ display: "flex", marginTop: "12px" }}>
           <Button
             sx={{ marginRight: "12px", fontFamily: "Roboto Mono" }}
@@ -45,8 +48,18 @@ function SelectRoomDialog({ open, handleClose }) {
             sx={{ color: "white", fontFamily: "Roboto Mono" }}
             variant="contained"
             disabled={!roomId}
-            onClick={() => {
-              navigate(`room/${roomId}`);
+            onClick={ () => {
+              axios.post(
+                "http://localhost:3001/find-room-with-id", {
+                  "roomId": roomId,
+                }
+              )
+              .then((response) => {
+                const roomId = response.data["foundRoomIds"][0];
+                navigate(`room/${roomId}`);
+              }).catch((error) => {
+                setError(error.response.data);
+              });
             }}
           >
             Go
